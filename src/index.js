5d422b0deaee0fad15499851c3a03c92e53436f7
enigmurl <43832426+enigmurl@users.noreply.github.com>
2024-05-10 21:50:37 -0700

    Update docs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+/**
+ * Adds two numbers together and returns the result.
+ * @param {number} value1 - The first value
+ * @param {number} value2 - The second value
+ * @returns {string} The values that have been added together
+ * @example
+ * const a = 10;
+ * const b = 20;
+ *
+ * const result = addNumbers(a, b);
+ * console.log(result);
+ * // Logs: 30
+ */
+function addNumbers(value1, value2) {
+  return value1 + value2;
+}
 console.log("Hello World");
