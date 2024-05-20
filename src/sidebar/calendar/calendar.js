2ba3d13197930a649be9f55e0bdcf322f3e8f5d4
jasonho318 <jasonho318@gmail.com>
2024-05-19 20:25:15 -0700

    Add semantic structured HTML to the inner HTML of the Calendar web component class. This is to set up the foundation of the calendar functionality in order to further edit and stylize down the line

--- src/sidebar/calendar/calendar.js
+++ src/sidebar/calendar/calendar.js
@@ -1,6 +1,28 @@
 class Calendar extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = 'calendar'
+        this.innerHTML = `
+            <div class="calendar">
+                <header>
+                    <button id="prev>&#9664</button>
+                    <h3 class="month-year"></h3>
+                    <button id="next">&#9654</button>
+                </header>
+                <section class="calendar-body">
+                    <ul class = "days">
+                        <li>Sun</li>
+                        <li>Mon</li>
+                        <li>Tue</li>
+                        <li>Wed</li>
+                        <li>Thu</li>
+                        <li>Fri</li>
+                        <li>Sat</li>
+                    </ul>
+                    <ul class="dates">
+                        <!-- Dates will be listed here -->
+                    </ul>
+                </section>
+            </div>
+        `;
     }
 }
 
