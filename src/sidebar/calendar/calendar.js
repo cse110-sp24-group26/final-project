7550ef95598ae6e0985c19a29e07a180e4a6f46a
jasonho318 <jasonho318@gmail.com>
2024-05-19 21:59:25 -0700

    Add functionality for calendar to parse dates accurately and display them properly. Align elements properly.

--- src/sidebar/calendar/calendar.js
+++ src/sidebar/calendar/calendar.js
@@ -3,19 +3,19 @@ class Calendar extends HTMLElement {
         this.innerHTML = `
             <div class="calendar">
                 <header>
-                    <button id="prev>&#9664</button>
+                    <button id="prev">&#9664</button>
                     <h3 class="month-year"></h3>
                     <button id="next">&#9654</button>
                 </header>
                 <section class="calendar-body">
                     <ul class = "days">
-                        <li>Sun</li>
-                        <li>Mon</li>
-                        <li>Tue</li>
-                        <li>Wed</li>
-                        <li>Thu</li>
-                        <li>Fri</li>
-                        <li>Sat</li>
+                        <li>S</li>
+                        <li>M</li>
+                        <li>T</li>
+                        <li>W</li>
+                        <li>T</li>
+                        <li>F</li>
+                        <li>S</li>
                     </ul>
                     <ul class="dates">
                         <!-- Dates will be listed here -->
@@ -23,6 +23,31 @@ class Calendar extends HTMLElement {
                 </section>
             </div>
         `;
+
+        this.updateCalendar(new Date());
+    }
+
+
+    /* This function  */
+    updateCalendar(date) {
+        const monthYearHeading = this.querySelector('.month-year');
+        const datesContainer = this.querySelector('.dates');
+
+        datesContainer.innerHTML = '';
+
+        const month = date.getMonth();
+        const year = date.getFullYear();
+        monthYearHeading.textContent = `${date.toLocaleString('default', { month: 'long' })} ${year}`;
+        const firstDay = new Date(year, month, 1).getDay();
+        const lastDay = new Date(year, month + 1, 0).getDate();
+
+        for (let i = 0; i < firstDay; i++) {
+            datesContainer.innerHTML += `<li class="date empty"></li>`;
+        }
+
+        for (let i = 1; i <= lastDay; i++) {
+            datesContainer.innerHTML += `<li class="date">${i}</li>`;
+        }
     }
 }
 
