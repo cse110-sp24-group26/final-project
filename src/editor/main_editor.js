3eb00bafa43e4e7149c96a09578d357f77b06ee7
Suhaib Chowdhury <ogs1492@gmail.com>
2024-05-25 14:05:25 -0700

    Fixed editor size

--- src/editor/main_editor.js
+++ src/editor/main_editor.js
@@ -19,28 +19,34 @@ class MainEditor extends HTMLElement {
 		iframe.addEventListener("load", () => {
 			let iframeDoc = iframe.contentDocument;
 			let div = iframeDoc.createElement('div');
+			div.style.height = "100%"
 			div.setAttribute('contenteditable', "true");
 			iframeDoc.body.appendChild(div);
-			div.addEventListener('keyup', update)
 		})
 
-		function update() {
+		/*function update() {
+			render();
+		}
+
+		function removeStyleTags(text){
+			return text.replace('</.b>', "")
+		}
+
+		/*function render(){
 			let iframeDoc = iframe.contentDocument;
 			let div = iframeDoc.getElementsByTagName('div')[0];
-			let text = div.textContent;
-			let changed = false;
+			let text = div.innerHTML;
+			text = removeStyleTags(text);
+			console.log(text);
 			if (text.includes("__")){
-				console.log(text)
+				console.log(text);
+				console.log(div.innerHTML);
 				let m = text.match(/__.+__/);
-				let new_text = `<b>` + m + `</b>`
-				text = text.replace(m, new_text)
-				console.log(div.innerHTML)
-				changed = true;
-			}
-			if (changed == true){
+				let new_text = `<b>` + m + `</b>`;
+				text = text.replace(m, new_text);
 				div.innerHTML = text;
 			}
-		}
+		}*/
 	}
 }
 
