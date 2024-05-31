3cade21379aba0a165693986692f0e4c03db89e0
Santiago-Duque <103283657+Santiago-Duque@users.noreply.github.com>
2024-05-30 19:27:57 -0700

    Create B+I+H button, B,I Partially Function

--- src/editor/editor_tools.js
+++ src/editor/editor_tools.js
@@ -1,7 +1,64 @@
 class EditorTools extends HTMLElement {
-	connectedCallback() {
-		this.innerHTML = "Editor Tools"
-	}
+    connectedCallback() {
+        const shadow = this.attachShadow( {mode : "open"});
+        shadow.innerHTML = `
+            <button id = "bold-button"><b>B</b></button>
+            <button id = "italic-button"><i>I</i></button>
+            <button id = "header-button">H</button>
+        `;
+            //figure out a click counter so when buttons are clicked twice it undo-s the bold/italic/header
+            const boldButton = shadow.querySelector('#bold-button');
+            const italicButton = shadow.querySelector('#italic-button');
+   
+            boldButton.addEventListener('click', (event) => {
+                event.preventDefault();
+                const mainEditor = document.querySelector('m-main-editor');
+                const textArea = mainEditor.querySelector('#text-content');
+                console.log("button clicked");
+                const text = shadow.getSelection().toString();
+                console.log(text);
+                const formattedText = `**${text}**`;
+                console.log(formattedText);
+                console.log(textArea);
+                const replaced = textArea.innerHTML.replace(text, formattedText);
+                textArea.innerHTML = replaced;
+                selection.removeAllRanges();
+            });
+   
+            italicButton.addEventListener('click', (event) => {
+                event.preventDefault();
+                const mainEditor = document.querySelector('m-main-editor');
+                const textArea = mainEditor.querySelector('#text-content');
+                console.log("button clicked");
+                const text = shadow.getSelection().toString();
+                console.log(text);
+                const formattedText = `*${text}*`;
+                console.log(formattedText);
+                console.log(textArea);
+                const replaced = textArea.innerHTML.replace(text, formattedText);
+                textArea.innerHTML = replaced;
+                selection.removeAllRanges();
+            });
+
+            headerButton.addEventListener('click', (event) => {
+                event.preventDefault();
+                const mainEditor = document.querySelector('m-main-editor');
+                const textArea = mainEditor.querySelector('#text-content');
+                console.log("button clicked");
+                const text = shadow.getSelection().toString();
+                console.log(text);
+                const formattedText = `*${text}*`; //change the format to a heading syntax
+                console.log(formattedText);
+                console.log(textArea);
+                const replaced = textArea.innerHTML.replace(text, formattedText);
+                textArea.innerHTML = replaced;
+                selection.removeAllRanges();
+            });
+
+    }
+
+
 }
+
 
 customElements.define('m-editor-tools', EditorTools);
