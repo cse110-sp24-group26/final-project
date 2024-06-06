20782109d66ddf9ce365dd071bb58fe573f01e2c
jasonho318 <jasonho318@gmail.com>
2024-06-05 22:02:02 -0700

    Calendar initial state component tests

--- cypress/component/calendar.cy.js
+++ cypress/component/calendar.cy.js
@@ -9,16 +9,61 @@ describe('Initial Load Calendar Tests', () => {
         return false;
     });
 
-    it ('Initial Calendar month, years, and days', () => {
+    it ('Initial Calendar month', () => {
+        const date = new Date();
+        const currentMonth = date.getMonth();
 
+        cy.get('#month').invoke('val').then((monthNum) => {
+            expect(monthNum).to.equal(currentMonth.toString());
+        });
     })
 
-    it('Initial selected calendar day', () => {
+    it('Initial Calendar year', () => {
+        const date = new Date();
+        const currentYear = date.getFullYear();
 
+        cy.get('#year').invoke('val').then((value) => {
+            cy.log("Selected value is: " + value);
+            expect(value).to.equal(currentYear.toString());
+        });
     })
 
-    it('Selecting a new date', () => {
+    it('Initial current month dates', () => {
+        const date = new Date();
+        const month = date.getMonth();
+        const year = date.getFullYear();
 
+        const start = new Date(year, month, 1).getDay();
+        const endDate = new Date(year, month + 1, 0).getDate();
+        const end = new Date(year, month, endDate).getDay();
+        const endDatePrev = new Date(year, month, 0).getDate();
+        const expectedItems = [];
+
+        let added = 0;
+
+        for (let i = start; i > 0; i--) {
+            expectedItems.push((endDatePrev- i + 1).toString());
+            added += 1;
+        }
+
+        for (let i = 1; i <= endDate; i++) {
+            expectedItems.push(i.toString());
+            added += 1;
+        }
+
+        for (let i = end; i < 6; i++) {
+            expectedItems.push((i - end + 1).toString());
+            added += 1;
+        }
+
+        cy.get('.dates li').each((item, index) => {
+            cy.wrap(item).should('have.text', expectedItems[index]);
+        });
+    })
+
+    it('Initial currently selected date', () => {
+        const todaysDate = new Date().getDate();
+        cy.get(`.dates li.${'today'} button.${'selected'}`).should('have.text', todaysDate.toString());
     })
 })
 
